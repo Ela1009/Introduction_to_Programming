d97eff01deabe88322f2ca07b66ea9f57804c752
Ela1009 <ela.vrcelj@gmail.com>
2023-08-27 18:35:57 +0200

    Update The program reads numbers from the user and saves them in a list. When the user types 0, the program asks for an additional number N and prints it.py

--- The program reads numbers from the user and saves them in a list. When the user types 0, the program asks for an additional number N and prints it.py
+++ The program reads numbers from the user and saves them in a list. When the user types 0, the program asks for an additional number N and prints it.py
@@ -1,11 +1,15 @@
-def funkcija(lista):
-    nova=[]
-    for i in range (len(lista)):
-        for j in range(i):
-            if lista[i]%lista[j]!=0:
-                break
-        nova.append(lista[i])
-             
-    return nova
+lst = []
+zbroj = 0
 
-print(funkcija([2,3,6,12,15]))
+while True:
+    br = int(input("Unesite broj koji zelite unijeti u listu: "))
+    lst.append(br)
+    if br == 0:
+        N = int(input("Upisite dodatni broj N: "))
+        if N > len(lst):
+            print("Greska! Nema dovoljno brojeva u listi.")
+        else:
+            for i in range(len(lst) - N, len(lst)):
+                zbroj += lst[i]
+            print("Zbroj zadnjih", N, "upisanih brojeva: ", zbroj)
+        break
