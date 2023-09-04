139d6910f371d3fd06d319ba1e38c60ab4423ec0
Ela1009 <ela.vrcelj@gmail.com>
2023-09-04 20:44:44 +0200

    Create The value in the list is True only if that number is divisible by the next number.py

---
+++ The value in the list is True only if that number is divisible by the next number.py
@@ -0,0 +1,11 @@
+def vratibool(lst):
+    b = []
+    for i in range(len(lst) - 1):
+        if lst[i] % lst[i + 1] == 0:
+            b.append(True)
+        else:
+            b.append(False)
+    b.append(False) #za zadnji el. u listi uvik dodat jos jedan False, jer nema sljedbenika
+    return b
+
+print(vratibool([6, 3, 1, 4, 2, 4]))
